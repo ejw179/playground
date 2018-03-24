d2dee596827d828dac191b200f4cbb71be1a5cab
Your Mom <ejw179@gmail.com>
2018-03-23 20:47:56 -0400

    Day 1.222222

---
+++ gitcmd.ps1
@@ -0,0 +1,33 @@
+﻿<#
+Git
+#>
+
+$gitRepo = $env:USERPROFILE + "\dropbox\git\"
+cd $gitRepo
+# Play ground Repo - https://github.com/ejw179/playground.git 
+# GEt Repo
+Set-ExecutionPolicy RemoteSigned -Force -
+git clone https://github.com/ejw179/playground.git
+#Edit and Update
+code .
+git status
+git add .
+git status
+git commit -m "Day 1.1"
+
+# Update from github
+git fetch
+git pull
+git log
+
+# Update Git Hub
+git push
+
+<#
+Posh-Git
+#>
+git clone https://github.com/dahlbyk/posh-git.git
+cd .\posh-git
+.\install.ps1
+
+
